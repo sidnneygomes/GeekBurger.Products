1e8b985fc42b9f4d1625fea3d058ce71d80a2b21
Gabriel Oliveira <36790447+OliveiraGa@users.noreply.github.com>
2023-03-27 22:03:28 -0300

    ServiceBusTopic

---
+++ ServiceBusTopic/ProductChangedService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ServiceBusTopic
+{
+    public class ProductChangedService : IProductChangedService
+    {
+        public void AddToMessageList
+           (IEnumerable<EntityEntry<Product>> changes)
+        {
+            _messages.AddRange(changes
+                .Where(entity =>
+                  entity.State != EntityState.Detached
+                  && entity.State != EntityState.Unchanged)
+                .Select(entity => GetMessage(entity)));
+        }
+
+        private Message GetMessage(EntityEntry<Product> entity)
+        {
+            var productChanged =
+                Mapper.Map<ProductChanged>(entity);
+            var productChangedSerialized =
+                JsonConvert.SerializeObject(productChanged);
+            var productChangedByteArray =
+               Encoding.UTF8.GetBytes(productChangedSerialized);
+
+            return new Message
+            {
+                Body = productChangedByteArray,
+                MessageId = Guid.NewGuid().ToString(),
+                Label = productChanged.Product.StoreId.ToString()
+            };
+        }
+    }
+
+    public interface IProductChangedService
+    {
+    }
+}
---
+++ ServiceBusTopic/ServiceBusConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ServiceBus.Fluent;
+
+namespace ServiceBusTopic
+{
+    public class ServiceBusConfiguration
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string TenantId { get; set; }
+        public string SubscriptionId { get; set; }
+        public string ResourceGroup { get; set; }
+        public string NamespaceName { get; set; }
+        public string ConnectionString { get; set; }
+
+    }
+
+    public static class GetServiceBusConfigurantion
+    {
+        public static IServiceBusNamespace GetServiceBusNamespace(this IConfiguration configuration)
+        {
+            var config = configuration.GetSection("serviceBus")
+                         .Get<ServiceBusConfiguration>();
+
+            var credentials = SdkContext.AzureCredentialsFactory
+                .FromServicePrincipal(config.ClientId,
+                               config.ClientSecret,
+                               config.TenantId,
+                               AzureEnvironment.AzureGlobalCloud);
+
+            var serviceBusManager = ServiceBusManager
+                .Authenticate(credentials, config.SubscriptionId);
+            return serviceBusManager.Namespaces
+                   .GetByResourceGroup(config.ResourceGroup,
+                   config.NamespaceName);
+        }
+
+    }
+}
